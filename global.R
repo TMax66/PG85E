72e72d70e81f853cc86b3ec1bb39b9e4f8d3ee73
TMax66 <39553077+TMax66@users.noreply.github.com>
2019-10-20 22:28:59 +0200

    update

--- global.R
+++ global.R
@@ -4,30 +4,36 @@ library(dplyr)
 library(tidyr)
 library(googlesheets)
 library(ggplot2)
-library(rpivotTable)
-library(timevis)
 library(janitor)
 library(kableExtra)
 library(knitr)
 library(formattable)
+library(readxl)
 
-Sys.setlocale("LC_ALL", "C")
-sheet <- gs_title("pg85e")
-dt<-gs_read(sheet, ws="pg85e" )
-r<-gs_read(sheet, ws="r")
+#
 
-
-tab<-full_join(dt,r,by="MP")
-names(tab)[7]<-"r"
-
-tab<-tab %>%
-  group_by(Anno, Operatore, MP, Nconf,r, esito1, esito2) %>% 
-  summarise("ripetibilità"=abs(log10(esito1)-log10(esito2))) %>% 
-  mutate("Giudizio"=ifelse(ripetibilità<=r, "Favorevole", "Sfavorevole"))
-  
+# sheet <- gs_title("pg85e")
+# dt<-gs_read(sheet, ws="pg85e" )
+# r<-gs_read(sheet, ws="r")
 
 
+# dt <- as_tibble(read_excel("pg85e.xlsx"))
+# 
+# r <- as_tibble(read_excel("rip.xlsx"))
 
+
+# tab<-full_join(dt,r,by="MP")
+# names(tab)[7]<-"r"
+# 
+# tab<-tab %>%
+#   group_by(Anno, Operatore, MP, Nconf,r, esito1, esito2) %>% 
+#   summarise("ripetibilità"=abs(log10(esito1)-log10(esito2))) %>% 
+#   mutate("Giudizio"=ifelse(ripetibilità<=r, "Favorevole", "Sfavorevole"))
+# 
+# 
+# write.table(tab, file="dati.csv")  
+
+tab<-read.csv("dati.csv", sep="")
 
 
 
